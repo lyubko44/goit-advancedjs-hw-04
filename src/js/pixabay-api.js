1aee48c9737c636f9ed8275e3c158369b40fd7a7
Liubomyr Skorokhod <lyubko44@gmail.com>
2024-10-22 00:15:51 -0700

    use axios and async code for fetch images

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,9 +1,20 @@
+import axios from 'axios';
+
 const apiKey = '46523265-c71f09449dcc8bc0189ac0d2d';
 
-const fetchImages = (searchParam, page) => {
-  return fetch(
-    `https://pixabay.com/api/?key=${apiKey}&q=${searchParam}&image_type=photo&orientation=horizontal&safesearch=true&per_page=12&page=${page}`
-  );
-};
+async function fetchImages(searchParam, perPage = 15, page = 1) {
+  const params = new URLSearchParams({
+    key: apiKey,
+    q: searchParam,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: 'true',
+    per_page: perPage,
+    page: page,
+  });
+
+  const response = await axios.get(`https://pixabay.com/api/?${params}`);
+  return response.data;
+}
 
 export default fetchImages;
--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 import fetchImages from './js/pixabay-api';
-import { renderLoading, renderImages, renderError} from './js/render-functions';
+import { renderLoading, renderImages, renderError } from './js/render-functions';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
@@ -15,44 +15,70 @@ const simpleGallery = new SimpleLightbox('.gallery a', {
 });
 
 const searchForm = document.querySelector('.search-form');
+const loadMoreBtn = document.querySelector('.load-more');
+const gallery = document.querySelector('.gallery');
 
-searchForm.addEventListener('submit', event => {
+let page = 1;
+let perPage = 15;
+let searchParam = '';
+let totalHits = 0;
+
+searchForm.addEventListener('submit', async event => {
   event.preventDefault();
 
   const form = event.target;
-
-  const searchParam = form.elements.search.value.trim();
+  searchParam = form.elements.search.value.trim();
   if (!searchParam) {
     return;
   }
 
   form.reset();
-
+  page = 1;
   renderLoading();
 
-  fetchImages(searchParam, 1)
-    .then(response => {
-      return response.json();
-    })
-    .then(data => {
-      if (data.total === 0) {
-        return Promise.reject(
-          new Error(
-            'Sorry, there are no images matching your search query. Please try again!'
-          )
-        );
-      } else {
-        renderImages(data.hits);
-      }
-    })
-    .then(() => {
-      simpleGallery.refresh();
-    })
-    .catch(error => {
-      iziToast.error({
-        title: 'Error',
-        message: error.message,
-      });
-      renderError();
+  try {
+    const images = await fetchImages(searchParam, perPage, page);
+    totalHits = images.totalHits;
+    if (images.total === 0) {
+      throw new Error('Sorry, there are no images matching your search query. Please try again!');
+    }
+    renderImages(images.hits);
+    simpleGallery.refresh();
+    loadMoreBtn.style.display = 'flex';
+  } catch (error) {
+    iziToast.error({
+      title: 'Error',
+      message: error.message,
     });
+    renderError();
+  }
+});
+
+loadMoreBtn.addEventListener('click', async () => {
+  page += 1;
+  try {
+    const images = await fetchImages(searchParam, perPage, page);
+    renderImages(images.hits);
+    simpleGallery.refresh();
+
+    const { height: cardHeight } = gallery.firstElementChild.getBoundingClientRect();
+    window.scrollBy({
+      top: cardHeight * 2,
+      behavior: 'smooth',
+    });
+
+    if (page * perPage >= totalHits) {
+      loadMoreBtn.style.display = 'none';
+      iziToast.info({
+        title: 'Info',
+        message: "We're sorry, but you've reached the end of search results.",
+      });
+    }
+  } catch (error) {
+    iziToast.error({
+      title: 'Error',
+      message: error.message,
+    });
+    renderError();
+  }
 });
